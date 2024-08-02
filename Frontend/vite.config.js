cb6f3ace9d51b18bbc4c5d3f84d71cee103e9d58
Vishwas katiyar <vishwaskatiyar39@gmail.com>
2024-08-02 22:14:32 +0530

    hoated api connected

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:10000',
+        target: 'https://twitter-clone-full-stack-0ru5.onrender.com',
         changeOrigin: true,
       },
     }
