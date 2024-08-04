d57adba358e011d2b905073dbddcb257bf3a1d78
Vishwas katiyar <154349908+vishwaskatiyar@users.noreply.github.com>
2024-08-05 00:26:52 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,3 +1,4 @@
+import path from "path"
 import express from "express";
 import dotenv from "dotenv"
 import cookieParser from "cookie-parser";
@@ -24,11 +25,11 @@ const app = express();
 //     origin: 'https://twitter-clone-full-stack-lovat.vercel.app/',
 // }));
 
-app.use(cors({
-    origin: 'http://localhost:5173',
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
-}))
+// app.use(cors({
+//     origin: 'http://localhost:3000',
+//     credentials: true,
+//     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
+// }))
 
 
 // app.get('/', (req, res) => {
@@ -36,14 +37,23 @@ app.use(cors({
 // });
 // console.log(process.env.MONGO_URI);
 const PORT = process.env.PORT || 10000;
+const __dirname = path.resolve()
 
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/notification", notificationRoutes);
+
+if (process.env.NODE_ENV == "production") {
+    app.use(express.static(path.join(__dirname, "Frontend/dist")))
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "Frontend", "dist", "index.html"))
+    })
+}
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
